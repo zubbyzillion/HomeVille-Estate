19f1443605802364286ffd8d24084c1664012c41
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-02-09 10:54:16 +0100

    Adding Header

---
+++ src/components/common/header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="logo"></div>
+                </div>
+            </header>
+        </>
+    )
+}
