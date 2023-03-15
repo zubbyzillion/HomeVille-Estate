dbbdef1d2ec1bed911d909aeaf028315bbfa6ae7
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-15 19:04:27 +0100

    Header - Media Queries

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import { nav } from "../../data/Data";
 
 const Header = () => {
+    const [navlist, setnavlist] = useState(false)
     return (
         <>
             <header>
@@ -12,7 +13,7 @@ const Header = () => {
                         <img src="./images/logo.png" alt="" />
                     </div>
                     <div className="nav">
-                        <ul className="flex">
+                        <ul className={navlist ? "small" : "flex"}>
                             {nav.map((list, index) => (
                             <li key={index}>
                                 <Link to={list.path}>{list.text}</Link>
@@ -29,11 +30,11 @@ const Header = () => {
                         </button>
                     </div>
 
-                    {/* <div className="toggle">
-                        <button>
-                            <i className="fa fa-bars"></i>
+                    <div className="toggle">
+                        <button onClick={() => setnavlist(!navlist)}>
+                            {navlist ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
                         </button>
-                    </div> */}
+                    </div>
                 </div>
             </header>
         </>
