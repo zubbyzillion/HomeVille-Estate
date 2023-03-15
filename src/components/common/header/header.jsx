674c6e0a63dce6ec1bf5c553bdd8535aa0a38612
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-15 15:42:53 +0100

    Header

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -19,6 +19,14 @@ const Header = () => {
                             ))}
                         </ul>
                     </div>
+                    <div className="button flex">
+                        <h4>
+                            <span>2</span> My List
+                        </h4>
+                        <button className="btn1">
+                            <i className="fa fa-sign-out"></i>
+                        </button>
+                    </div>
                 </div>
             </header>
         </>
