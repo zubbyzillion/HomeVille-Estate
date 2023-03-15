153061d3a7d3bc97b7b4c450f1d53adab9825c42
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-15 17:21:35 +0100

    Header - Styling

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./header.css";
 import { Link } from "react-router-dom";
 import { nav } from "../../data/Data";
 
@@ -24,9 +25,15 @@ const Header = () => {
                             <span>2</span> My List
                         </h4>
                         <button className="btn1">
-                            <i className="fa fa-sign-out"></i>
+                            <i className="fa fa-sign-out"> </i> Sign In
                         </button>
                     </div>
+
+                    {/* <div className="toggle">
+                        <button>
+                            <i className="fa fa-bars"></i>
+                        </button>
+                    </div> */}
                 </div>
             </header>
         </>
