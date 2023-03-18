799c2ef9ff21101304cb3776d0861155dded38d3
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-18 15:10:41 +0100

    Header Styling

--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../common/header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../home/Home";
 
 const Pages = () => {
     return (
