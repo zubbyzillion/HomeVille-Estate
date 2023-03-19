371df18f4b8b1c470e0e2fc02613d4c5da461627
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-19 15:32:01 +0100

    Recent

--- src/components/Home/featured/FeaturedCard.jsx
+++ src/components/Home/featured/FeaturedCard.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { featured } from "../../data/Data";
 
 const FeaturedCard = () => {
     return (
-        <div>FeaturedCard</div>
+        <>
+            <div className="content grid5 mtop">
+                {featured.map((items, index) => (
+                    <div className="box" key={index}>
+                        <img src={items.cover} alt="" />
+                        <h4>{items.name}</h4>
+                        <label>{items.total}</label>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
