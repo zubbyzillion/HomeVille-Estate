fee21e602e68f2faee2bdb7eaebfca640c303005
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-19 14:51:55 +0100

    FeaturedCard

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "./hero/Hero";
+import Featured from "./featured/Featured";
 
 const Home = () => {
     return (
--- src/components/Home/featured/Featured.jsx
+++ src/components/Home/featured/Featured.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Heading from "../../common/Heading";
 import "./featured.css"
 
 const Featured = () => {
     return (
-        <>Featured</>
+        <> 
+             <section className="featured background">
+              <div className="container">
+                <Heading title="Featured Property Types" subtitle="Find All Type of Property." />
+                <FeaturedCard />
+              </div>
+             </section> 
+        </>
     )
 }
 
