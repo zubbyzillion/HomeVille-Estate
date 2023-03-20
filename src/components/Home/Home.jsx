653ca1502df44158db7ff860e6e56632c5997a9f
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-20 18:26:45 +0100

    Recent Styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/Hero";
 import Featured from "./featured/Featured";
 import Recent from "./recent/Recent";
+// import Location from "./location/Location";
 
 const Home = () => {
     return (
