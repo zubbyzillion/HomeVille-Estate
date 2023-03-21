13a61ea7a349ffa50f6751ef57445e69a44f8de4
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-21 18:50:19 +0100

    Added Awards Components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/Hero";
 import Featured from "./featured/Featured";
 import Recent from "./recent/Recent";
+import Awards from "./awards/Awards";
 // import Location from "./location/Location";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Hero />
             <Featured />
             <Recent />
+            <Awards />
         </>
     )
 }
