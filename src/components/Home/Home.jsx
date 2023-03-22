ca31eba69eeeeda375e72f617d4d5cf730ec1aa4
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-22 22:34:18 +0100

    Fixed Overlay Styling Issues

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Featured from "./featured/Featured";
 import Recent from "./recent/Recent";
 import Awards from "./awards/Awards";
 import Location from "./location/Location";
+// import Team from ".team/Team";
 
 const Home = () => {
     return (
