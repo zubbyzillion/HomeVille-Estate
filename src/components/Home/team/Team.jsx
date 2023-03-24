37600d94d51a2d3ce8388970173c6cf01ae88fe1
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-24 19:15:17 +0100

    Added Team Components and Data

--- src/components/Home/team/Team.jsx
+++ src/components/Home/team/Team.jsx
@@ -15,6 +15,29 @@ const Team = () => {
                         return (
                             <div className="box" key={index}>
                                 <button className="btn3">{val.list} Listings</button>
+                                <div className="details">
+                                    <div className="img">
+                                        <img src={val.cover} alt="" />
+                                        <i className="fa fa-circle-check"></i>
+                                    </div>
+                                    <i className="fa fa-location-dot"></i>
+                                    <label htmlFor="">{val.address}</label>
+                                    <h4>{val.name}</h4>
+
+                                    <ul>
+                                        {val.icon.map((icon, index) => (
+                                            <li key={index}> {icon} </li>
+                                        ))}
+                                    </ul>
+                                    <div className="button flex">
+                                        <button>
+                                            <i className="fa fa-envelope"></i> Message
+                                        </button>
+                                        <button className="btn4">
+                                            <i className="fa fa-phone-alt"></i> Message
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         )
                     })}
