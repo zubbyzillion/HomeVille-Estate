8bf605ec4570c8dc38336c76b2a3e1708cc3f5ca
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-03-31 18:24:28 +0100

    Added about folder

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Back from "../common/Back";
+import Heading from "../common/Heading";
+import img from "../images/about.jpg";
+import "./about.css";
+
+const About = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default About;
