91332d5f93058967682be253d78a6e5ee9d3440b
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-04-04 12:57:48 +0100

    Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import img from "../common/Back";
+import "../home/featured/Featured.css";
+import FeaturedCard from "../home//featured/FeaturedCard";
 
 const Services = () => {
   return (
     <>
         <section className="services mb">
-            <Back />
+            <Back name="Services" title="Services - All Services"/>
+            <div className="featured container">
+                <FeaturedCard />
+            </div>
         </section>
     </>
   )
