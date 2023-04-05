7f8495413818d3d313fa5e71d17fbc06c74e2742
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-04-05 17:25:04 +0100

    Added Blog folder

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import img from "../common/Back";
+import img from "../images/services.jpg";
+import Back from "../common/Back";
 import "../home/featured/Featured.css";
 import FeaturedCard from "../home/featured/FeaturedCard";
 
@@ -7,7 +8,7 @@ const Services = () => {
   return (
     <>
         <section className="services mb">
-            <Back name="Services" title="Services - All Services"/>
+            <Back name="Services" title="Services - All Services" cover={img}/>
             <div className="featured container">
                 <FeaturedCard />
             </div>
