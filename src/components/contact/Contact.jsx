e6ccd56cd65405f6ac0c7d14e8ac68068954f48f
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-04-07 08:58:02 +0100

    Contact folder structure

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import img from "../images/pricing.jpg";
+import Back from "../common/Back";
+import "./contact.css";
+
+const Contact = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Contact;
--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -7,7 +7,7 @@ import About from "../about/About";
 import Services from "../services/Services";
 import Blog from "../blog/Blog";
 import Pricing from "../pricing/Pricing";
-// import Contact from "../contact/Contact";
+import Contact from "../contact/Contact";
 
 const Pages = () => {
     return (
@@ -20,6 +20,7 @@ const Pages = () => {
                     <Route exact path="/services" component={Services} />
                     <Route exact path="/blog" component={Blog} />
                     <Route exact path="/pricing" component={Pricing} />
+                    <Route exact path="/contact" component={Contact} />
                 </Switch>
                 <Footer />
             </Router>
