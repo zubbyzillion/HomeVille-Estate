fd6ef94ccecf08f2a850aeeabccfc5233321e974
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-04-07 09:03:45 +0100

    Added form to Contact folder

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,21 @@ import "./contact.css";
 const Contact = () => {
   return (
     <>
-    
+        <section className="contact mb">
+            <Back name="Contact Us" title="Get Helps & Friendly Support" cover={img} />
+            <div className="container">
+                <form className="shadow">
+                    <h4>Fill Up <T></T>he Form</h4> <br />
+                    <div>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <input type="text" placeholder="Subject" />
+                    <textarea cols="30" rows="10"></textarea>
+                    <button>Submit Request</button>
+                </form>
+            </div>
+        </section>
     </>
   )
 }
